fb2aeb05bc7919019624af049f67c75775f8e259
Neil <pranjal.deb@ekodus.com>
2021-10-04 12:25:42 +0530

    admin Login

--- abhit_api/app/Http/Controllers/admin/AuthController.php
+++ abhit_api/app/Http/Controllers/admin/AuthController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use App\Common\Role;
 
 class AuthController extends Controller
 {
@@ -16,10 +17,11 @@ class AuthController extends Controller
             'password' => 'required',
         ]);
 
-        $credentials = $request->only('email', 'password');
-        if (Auth::attempt($credentials)) {
+        if (Auth::attempt(['email' => $request->email,  'password' => $request->password, 'role_id' => Role::Admin ])) {
+
             return redirect()->route('admin.dashboard')
-                        ->withSuccess('Signed in');
+                ->withSuccess('Signed in');
+
         } else {
             return redirect()->back()->withErrors(['Credentials doesn\'t match with our record'])->withInput($request->input());
         }
