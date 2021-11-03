bb7ea02fa29e148f288296aa20dbc7be9648a61c
Alok Deka <alok.deka@ekodus.com>
2021-11-03 13:51:26 +0530

    Render HTML in banner

--- abhit_api/resources/views/website/home.blade.php
+++ abhit_api/resources/views/website/home.blade.php
@@ -19,8 +19,8 @@
                                     <div class="home-slide"><img src="{{ asset($item->banner_image) }}"
                                             style="border-radius: 20px" class="w100"></div>
                                     <div class="home-desc">
-                                        <h2 class="heading-black">{{ $item->name }}</h2>
-                                        <p class="banner-para">{{ $item->description }}</p>
+                                        <h2 class="heading-black">{!! $item->name !!}</h2>
+                                        <p class="banner-para">{!! $item->description !!}</p>
                                         @if ($item->course_id != null)
                                             <div><a href="{{ route('website.course.details', ['id' => \Crypt::encrypt($item->course_id)]) }}"
                                                     target="_blank" class="about-view">View More</a></div>
